1b3e2a2fbdc22b977b3eb5a0437dff08eb758bca
Vinicius Marques <vini.type@gmail.com>
2023-04-24 14:01:57 +0200

    use leader number to switch buffers

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -2,24 +2,24 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<M-,>', ':BufferPrevious<CR>', opts)
-map('n', '<M-.>', ':BufferNext<CR>', opts)
+map('n', '<leader>,', ':BufferPrevious<CR>', opts)
+map('n', '<leader>.', ':BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<M-<>', ':BufferMovePrevious<CR>', opts)
-map('n', '<M->>', ' :BufferMoveNext<CR>', opts)
+map('n', '<leader><', ':BufferMovePrevious<CR>', opts)
+map('n', '<leader>>', ' :BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<M-1>', ':BufferGoto 1<CR>', opts)
-map('n', '<M-2>', ':BufferGoto 2<CR>', opts)
-map('n', '<M-3>', ':BufferGoto 3<CR>', opts)
-map('n', '<M-4>', ':BufferGoto 4<CR>', opts)
-map('n', '<M-5>', ':BufferGoto 5<CR>', opts)
-map('n', '<M-6>', ':BufferGoto 6<CR>', opts)
-map('n', '<M-7>', ':BufferGoto 7<CR>', opts)
-map('n', '<M-8>', ':BufferGoto 8<CR>', opts)
-map('n', '<M-9>', ':BufferGoto 9<CR>', opts)
-map('n', '<M-0>', ':BufferLast<CR>', opts)
+map('n', '<leader>1', ':BufferGoto 1<CR>', opts)
+map('n', '<leader>2', ':BufferGoto 2<CR>', opts)
+map('n', '<leader>3', ':BufferGoto 3<CR>', opts)
+map('n', '<leader>4', ':BufferGoto 4<CR>', opts)
+map('n', '<leader>5', ':BufferGoto 5<CR>', opts)
+map('n', '<leader>6', ':BufferGoto 6<CR>', opts)
+map('n', '<leader>7', ':BufferGoto 7<CR>', opts)
+map('n', '<leader>8', ':BufferGoto 8<CR>', opts)
+map('n', '<leader>9', ':BufferGoto 9<CR>', opts)
+map('n', '<leader>0', ':BufferLast<CR>', opts)
 -- Close buffer
-map('n', '<M-c>', ':BufferClose<CR>', opts)
+map('n', '<leader>c', ':BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout<CR>
 -- Close commands
