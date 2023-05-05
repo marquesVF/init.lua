bcabf3afe5efcf3b2dac2482ce6363e476ecf4e5
Vinicius Marques <vini.type@gmail.com>
2023-05-05 11:54:01 +0200

    add jest configuration

---
+++ after/plugin/jester.lua
@@ -0,0 +1,11 @@
+local jester = require("jester")
+
+jester.setup({
+  path_to_jest_run = './node_modules/.bin/jest',
+  path_to_jest_debug = './node_modules/.bin/jest',
+})
+
+vim.keymap.set("n", "<leader>lj", function() jester.run() end, {})
+vim.keymap.set("n", "<leader>rj", function() jester.run_file() end, {})
+vim.keymap.set("n", "<leader>dj", function() jester.debug() end, {})
+
