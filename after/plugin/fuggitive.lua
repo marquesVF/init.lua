fb4c331821d3461956e569dbb54759a93efa7d30
Vinicius Marques <vini.type@gmail.com>
2023-11-12 21:38:37 +0100

    add more plugins

--- after/plugin/fuggitive.lua
+++ after/plugin/fuggitive.lua
@@ -2,3 +2,11 @@ vim.keymap.set("n", "<leader>gib", ":Git blame <CR>")
 vim.keymap.set("n", "<leader>gid", ":Git diff <CR>")
 vim.keymap.set("n", "<leader>gim", ":Git mergetool <CR>")
 
+-- Enables :GBrowse
+vim.api.nvim_create_user_command(
+  'Browse',
+  function (opts)
+    vim.fn.system { 'open', opts.fargs[1] }
+  end,
+  { nargs = 1 }
+)
