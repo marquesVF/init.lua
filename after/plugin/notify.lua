dff07c3428ce322a92d8823edd3046f19c5f7de6
Vinicius Marques <vini.type@gmail.com>
2025-04-23 10:20:50 +0200

    add notify configuration

---
+++ after/plugin/notify.lua
@@ -0,0 +1,10 @@
+require("notify").setup({
+  background_colour = "#000000",
+  stages = "fade_in_slide_out",
+  timeout = 3000,
+  render = "default",
+  top_down = false,
+  position = "bottom_right",
+  max_width = 50,
+  max_height = 10,
+}) 
