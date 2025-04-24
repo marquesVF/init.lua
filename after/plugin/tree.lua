54c1c81128ca1d4acc0b96ab3529248a5478cf35
Vinicius Marques <vini.type@gmail.com>
2025-04-24 15:58:58 +0200

    Improve tree appearance and settings

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -8,25 +8,78 @@ vim.opt.termguicolors = true
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>it", vim.cmd.NvimTreeFindFile)
 
--- empty setup using defaults
+-- setup with defaults
 require("nvim-tree").setup({
   view = {
-    preserve_window_proportions = false
+    width = 40,
+    preserve_window_proportions = true,
+    side = "left",
+    mappings = {
+      custom_only = false,
+      list = {
+        -- Custom mappings can be added here
+      },
+    },
   },
   renderer = {
     full_name = false,
+    group_empty = true,
+    highlight_git = true,
+    highlight_opened_files = "all",
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+        folder_arrow = true,
+      },
+    },
   },
   actions = {
     open_file = {
       resize_window = false,
+      window_picker = {
+        enable = true,
+      },
     },
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
   },
 })
 
+-- Function to save and restore window size
+local function save_tree_size()
+  local tree = require('nvim-tree.view')
+  if tree.is_visible() then
+    local width = vim.fn.winwidth(tree.get_winnr())
+    vim.g.nvim_tree_width = width
+  end
+end
 
-local tree = require('nvim-tree.view')
+local function restore_tree_size()
+  local tree = require('nvim-tree.view')
+  if tree.is_visible() and vim.g.nvim_tree_width then
+    vim.cmd('vertical resize ' .. vim.g.nvim_tree_width)
+  end
+end
+
+-- Create autocommands to handle window size
+vim.api.nvim_create_autocmd({ "WinResized" }, {
+  pattern = "NvimTree_*",
+  callback = save_tree_size,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = "NvimTree_*",
+  callback = restore_tree_size,
+})
 
 -- show line numbers in the tree
+local tree = require('nvim-tree.view')
 tree.View.winopts.relativenumber = true
--- don't resize the window when opening a file from the tree viewer
-tree.View.preserve_window_proportions = true
