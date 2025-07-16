739bc2bf9493b5a94322b31145ab1f7f7892ca50
Vinicius Marques <vini.type@gmail.com>
2025-07-16 02:52:24 +0200

    refactor: notify position

--- after/plugin/notify.lua
+++ after/plugin/notify.lua
@@ -1,10 +1,8 @@
 require("notify").setup({
   background_colour = "#000000",
   stages = "fade_in_slide_out",
-  timeout = 3000,
+  timeout = 2000,
   render = "default",
-  top_down = false,
-  position = "bottom_right",
-  max_width = 50,
+  max_width = 60,
   max_height = 10,
 }) 
