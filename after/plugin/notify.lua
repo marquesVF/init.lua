b1192dc0229f03606413f516729bc261d6a8b587
Vinicius Marques <vini.type@gmail.com>
2025-07-31 19:41:23 +0200

    refactor(notify): display errors only

--- after/plugin/notify.lua
+++ after/plugin/notify.lua
@@ -2,7 +2,8 @@ require("notify").setup({
   background_colour = "#000000",
   stages = "fade_in_slide_out",
   timeout = 2000,
-  render = "default",
+  render = "compact",
   max_width = 60,
   max_height = 10,
+  level = vim.log.levels.ERROR,
 }) 
